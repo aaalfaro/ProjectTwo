f706a9c225e97eabdaee4c7dff1995dedae9068c
Daniel <danieleabramson@gmail.com>
2018-06-03 15:44:35 -0400

    More path updates

--- ProjectTwo/src/main/java/com/revature/runners/TestNGRunner.java
+++ ProjectTwo/src/main/java/com/revature/runners/TestNGRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
 		//"C:/Users/Administrator/.jenkins/workspace/ProjectTwo/ProjectTwo/src/test/java/Update.feature"
-		features = {"C:/Users/Administrator/.jenkins/workspace/ProjectTwo/src/test/java/Popup.feature","C:/Users/Administrator/.jenkins/workspace/ProjectTwo/src/test/java/Close.feature","C:/Users/Administrator/.jenkins/workspace/ProjectTwo/src/test/java/DropDown.feature","C:/Users/Administrator/.jenkins/workspace/ProjectTwo/src/test/java/Report.feature","C:/Users/Administrator/.jenkins/workspace/ProjectTwo/src/test/java/Create.feature","C:/Users/Administrator/.jenkins/workspace/ProjectTwo/src/test/java/Update.feature","C:/Users/Administrator/.jenkins/workspace/ProjectTwo/src/test/java/Delete.feature"},
+		features = {"C:/Users/Administrator/.jenkins/workspace/ProjectTwo/ProjectTwo/src/test/java/Popup.feature","C:/Users/Administrator/.jenkins/workspace/ProjectTwo/ProjectTwo/src/test/java/Close.feature","C:/Users/Administrator/.jenkins/workspace/ProjectTwo/ProjectTwo/src/test/java/DropDown.feature","C:/Users/Administrator/.jenkins/workspace/ProjectTwo/ProjectTwo/src/test/java/Report.feature","C:/Users/Administrator/.jenkins/workspace/ProjectTwo/ProjectTwo/src/test/java/Create.feature","C:/Users/Administrator/.jenkins/workspace/ProjectTwo/ProjectTwo/src/test/java/Update.feature","C:/Users/Administrator/.jenkins/workspace/ProjectTwo/ProjectTwo/src/test/java/Delete.feature"},
 		glue = {"com.revature.StepImplementation"}
 		)
 public class TestNGRunner extends AbstractTestNGCucumberTests{
